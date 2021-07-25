e52319435967cbabe018d06572d17633f576181c
Joaqim Planstedt <github@joaqim.xyz>
2021-07-25 23:27:26 +0200

    Removed react support in eslint-plugin-*

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "prettier"],
-  extends: ["airbnb-typescript", "prettier"],
+  extends: ["airbnb-typescript/base", "prettier"],
   parserOptions: {
     project: "./tsconfig.json",
   },
